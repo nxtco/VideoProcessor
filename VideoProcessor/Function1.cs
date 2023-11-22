fc5f1762a25e9de5ab8ea8c9ef40cdab16c0bc6f
Sungmin Park <s.a.park@avanade.com>
2023-11-22 21:05:11 +0900

    HttpFunctions to a separate file

--- VideoProcessor/Function1.cs
+++ VideoProcessor/Function1.cs
@@ -1,9 +1,7 @@
 using System.Collections.Generic;
-using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
-using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
@@ -31,20 +29,6 @@ namespace VideoProcessor
         {
             log.LogInformation("Saying hello to {name}.", name);
             return $"Hello {name}!";
-        }
-
-        [FunctionName("Function1_HttpStart")]
-        public static async Task<HttpResponseMessage> HttpStart(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestMessage req,
-            [DurableClient] IDurableOrchestrationClient starter,
-            ILogger log)
-        {
-            // Function input comes from the request content.
-            string instanceId = await starter.StartNewAsync("Function1", null);
-
-            log.LogInformation("Started orchestration with ID = '{instanceId}'.", instanceId);
-
-            return starter.CreateCheckStatusResponse(req, instanceId);
         }
     }
 }
